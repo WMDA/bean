13897ca616ab12f6d0862f10d6c72e8b9f619602
WMDA <p3dh0001@gmail.com>
2020-05-28 22:30:19 +0100

    changed file stucture

--- setup.py
+++ setup.py
@@ -1,3 +1,32 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-setup(name='bean', version='1.0.0', packages=['bean'])
+def read_requirements(file):
+    with open(file) as f:
+        return f.read().splitlines()
+
+def read_file(file):
+   with open(file) as f:
+        return f.read()
+
+long_description = read_file("README.md")
+version = read_file("VERSION")
+requirements = read_requirements("requirements.txt")
+
+setup(
+    name = 'bean',
+    version = version,
+    author = 'WMDA',
+    author_email = '',
+    url = 'https://github.com/WMDA/bean.git',
+    description = 'Wrapper for statistical functions in python',
+    long_description_content_type = "text/x-rst",  # If this causes a warning, upgrade your setuptools package
+    long_description = long_description,
+    license = "MIT license",
+    packages = find_packages(exclude=["test"]),  # Don't include test directory in binary distribution
+    install_requires = requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]  # Update these accordingly
+)
